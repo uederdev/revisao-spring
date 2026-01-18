baa46392068fb0d94e82212ffefb9687db6e127d
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-01-18 17:15:48 -0300

    Primeiro commit - Criação do projeto

---
+++ src/main/java/br/com/ueder/revisaospring/RevisaospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.ueder.revisaospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ueder/revisaospring/RevisaospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ueder.revisaospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
